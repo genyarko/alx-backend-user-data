5c7c79f83ea9b830fa06f0bf0568200416cca090
genyarko <115112347+genyarko@users.noreply.github.com>
2023-11-07 19:52:36 +0000

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -4,6 +4,18 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+@app.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized_endpoint():
+    """Endpoint to raise a 401 error using abort
+
+    This endpoint will raise a 401 error, and the error handler for 401
+    in app.py will handle it.
+
+    Returns:
+        Response: A JSON response indicating unauthorized access.
+    """
+    abort(401)
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
